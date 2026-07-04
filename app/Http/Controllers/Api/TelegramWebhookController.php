09cdeb878957378d7c79b3bd115e2b2fdb437ec8
tereshonkov <tereshonkov.dima@gmail.com>
2026-07-05 00:37:43 +0300

    fix(TelegramWebhookController): update handle method to return JsonResponse

--- app/Http/Controllers/Api/TelegramWebhookController.php
+++ app/Http/Controllers/Api/TelegramWebhookController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
 use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TelegramWebhookController extends Controller
 {
-    public function handle(): void
+    public function handle(): JsonResponse
     {
         Telegram::commandsHandler(true);
+        return response()->json(['ok' => true]);
     }
 }
