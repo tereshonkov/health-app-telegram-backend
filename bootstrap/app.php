5847f7e49e916338f93eea694e7788418c151f6a
tereshonkov <tereshonkov.dima@gmail.com>
2026-07-08 20:59:23 +0300

    fix(middleware): add redirect for guests to null in middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             HandleCors::class,
         ]);
+        $middleware->redirectGuestsTo(fn () => null);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
