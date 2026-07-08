1a43154205d7ff829b12320d7d3f1055ba7e0541
tereshonkov <tereshonkov.dima@gmail.com>
2026-07-08 21:06:16 +0300

    fix(middleware): remove guest redirection from middleware configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,6 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             HandleCors::class,
         ]);
-        $middleware->redirectGuestsTo(fn () => null);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
